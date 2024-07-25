f1eeff6691c2670443b270d7aae7c1da89549095
Murphy <42812404+realtmxi@users.noreply.github.com>
2024-07-25 11:54:39 +0800

    Update ContentView.swift
    Array-ifeld the cards with ForEach

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,33 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    let emojis: Array<String> = ["😀", "😂 ", "🙃", "😘", "😘"]
     var body: some View {
         HStack {
-            CardView()
-            CardView()
+            ForEach(emojis.indices, id: \.self) {index in
+                CardView(content: emojis[index])
+            }
+            
+//            CardView(content: emojis[0], isFaceUp: true)
+//            CardView(content: emojis[1])
+//            CardView(content: emojis[2])
         }
 //        VStack(content: {
 //            Image(systemName: "globe")
 //                .imageScale(.large)
 //                .foregroundStyle(.tint)
 //            Text("Hello, world!")
-//        })
+//        })       .foregroundColor(.orange)
         .padding()
+        .foregroundColor(.orange)
     }
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    var content: String
+    @State var isFaceUp: Bool = false
     
     var body: some View {
-        ZStack(content: {
-            RoundedRectangle(cornerRadius: 12)
-                .foregroundColor(.white)
-            RoundedRectangle(cornerRadius: 12)
-                .strokeBorder(lineWidth: 2)
-            Text("test").font(.largeTitle)
-        })
+        ZStack {
+            let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+            
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            } else {
+                base.fill()
+            }
+        }
+            .onTapGesture {
+                print("tapped")
+                isFaceUp.toggle()
+        }
     }
 }
 
