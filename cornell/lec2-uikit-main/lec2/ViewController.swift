4f03e7c8bbb0119ea2d87c3c0645a180c99c9496
Murphy <42812404+realtmxi@users.noreply.github.com>
2024-07-26 19:48:45 +0800

    Update ViewController.swift

--- cornell/lec2-uikit-main/lec2/ViewController.swift
+++ cornell/lec2-uikit-main/lec2/ViewController.swift
@@ -9,40 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // MARK: Properties(view)
+    // MARK: - Properties (view)
     
-    let nameLabel = UILabel()
+    // 1. Create the view property
+    private let nameLabel = UILabel()
+    private let profileImageView = UIImageView()
     
-    
+    // MARK: - viewDidLoad
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         view.backgroundColor = UIColor.white
         
-        setupLabel()
-        
+        // 6. Call setup function
+        setupNameLabel()
+        setupProfileImage()
     }
     
-    private func setupLabel() {
-        nameLabel.text = "Richie Sun"
-        nameLabel.font = UIFont.systemFont(ofSize: 24)
+    // MARK: - Set Up Views
+    
+    // 2. Setup the view
+    private func setupNameLabel() {
+        // 3. Configure the view's properties
+        nameLabel.text = "Vin"
+        nameLabel.font = .systemFont(ofSize: 24)
         nameLabel.textColor = UIColor.black
         
+        // 4. Add view as a subview and enable auto-layout
         view.addSubview(nameLabel)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        nameLabel
-            .translatesAutoresizingMaskIntoConstraints
-        = false
+        // 5. Set Up Constraints
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    private func setupProfileImage() {
+        profileImageView.image = UIImage(named: "vin_eating")
         
-        NSLayoutConstraint.activate([nameLabel.topAnchor
-            .constraint(equalTo:
-                                    view.safeAreaLayoutGuide
-                                    .topAnchor),
-                                     nameLabel.centerXAnchor
-            .constraint(equalTo: view.centerXAnchor)
+        view.addSubview(profileImageView)
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            profileImageView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 24),
+            profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            profileImageView.widthAnchor.constraint(equalToConstant: 256),
+            profileImageView.heightAnchor.constraint(equalToConstant: 256)
         ])
     }
 
-
 }
-
